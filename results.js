0f05a1b66baf0a8cd2f08aa4f1aecb00d1d7b634
Murat Levent <levent.murat@icloud.com>
2024-08-08 22:05:01 +0300

    added swap function to exchange currency pairs between them

--- results.js
+++ results.js
@@ -1,14 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     const params = new URLSearchParams(window.location.search);
-    const baseCurrency = params.get('base');
-    const targetCurrency = params.get('target');
+    let baseCurrency = params.get('base');
+    let targetCurrency = params.get('target');
     const amountInput = document.getElementById('amount');
     const conversionResult = document.getElementById('conversion-result');
+    const baseCurrencySpan = document.getElementById('base-currency');
+    const targetCurrencySpan = document.getElementById('target-currency');
+    const refreshLink = document.getElementById('refresh');
+    const swapButton = document.getElementById('swap');
 
     if (!baseCurrency || !targetCurrency) {
         conversionResult.textContent = 'Please select both base and target currencies.';
         return;
     }
+
+    baseCurrencySpan.textContent = `${baseCurrency}`;
+    targetCurrencySpan.textContent = `${targetCurrency}`;
 
     function fetchExchangeRate() {
         fetch(`https://v6.exchangerate-api.com/v6/ec08bfe392aaa50ccb5e87f9/pair/${baseCurrency}/${targetCurrency}`)
@@ -30,6 +37,17 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     amountInput.addEventListener('input', fetchExchangeRate);
+    refreshLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        fetchExchangeRate();
+    });
+
+    swapButton.addEventListener('click', function() {
+        [baseCurrency, targetCurrency] = [targetCurrency, baseCurrency];
+        baseCurrencySpan.textContent = `${baseCurrency}`;
+        targetCurrencySpan.textContent = `${targetCurrency}`;
+        fetchExchangeRate();
+    });
 
     fetchExchangeRate();
 });
